f791684b1743df5b9c6e8923e5da09dd22cc6b1f
rasune <rasaz669@gmail.com>
2025-12-16 11:39:36 +0200

    Used Case statment for sales channel

--- adv_megatask.sql
+++ adv_megatask.sql
@@ -4,22 +4,26 @@
  -- sales_salesorderdetails, sale_slaeterritory ir sales_salesorderheader lenteles
  -- 1.susiek produktus   su ju subkategorijomis, kategorijoms, teritorijomis  ir pardavimo
  --  kanalais 
- SELECT pp.productID,
-    pp.Name AS produktas,
-    ps.ProductSubcategoryID,
-    ps.Name AS sub_kategorija,
-    pc.ProductCategoryID,
-    pc.Name AS kategorija,
-    st.Name AS teritorija,
-    ss.SalesOrderID,
-    soh.Status,
-    soh.OnlineOrderFlag
+  SELECT pp.productID,
+	pp.Name AS produktas,
+	ps.ProductSubcategoryID,
+	ps.Name AS sub_kategorija,
+	pc.ProductCategoryID,
+	pc.Name AS kategorija,
+	st.Name AS teritorija,
+	ss.SalesOrderID,
+	soh.Status,
+	soh.OnlineOrderFlag,
+	CASE 
+		WHEN soh.OnlineOrderFlag = 1 THEN  'online' 
+		ELSE 'direct' 
+    END AS sales_channel
  FROM  production_product pp
-    JOIN production_productsubcategory ps ON ps.ProductSubcategoryID = pp.ProductSubcategoryID
-    JOIN production_productcategory pc ON ps.ProductCategoryID = pc.ProductCategoryID
-    JOIN sales_salesorderdetail ss  ON pp.productID = ss.ProductID
-    JOIN sales_salesorderheader soh  ON ss.salesorderID = soh.salesorderID
-    JOIN sales_salesterritory st ON  soh.TerritoryID = st.TerritoryID
+	JOIN production_productsubcategory ps ON ps.ProductSubcategoryID = pp.ProductSubcategoryID
+	JOIN production_productcategory pc ON ps.ProductCategoryID = pc.ProductCategoryID
+	JOIN sales_salesorderdetail ss  ON pp.productID = ss.ProductID
+	JOIN sales_salesorderheader soh  ON ss.salesorderID = soh.salesorderID
+	JOIN sales_salesterritory st ON  soh.TerritoryID = st.TerritoryID
 
  --2. Apskaičiuoti bendras pajama bei runningtotal kiekvienai kategorijai pagal metus ir ketvirčius.
 
